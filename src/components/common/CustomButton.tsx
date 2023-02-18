0d6743a5032c2abfb662e5e10bbe9d173c413687
Francis Ong <francis.ong@stratpoint.com>
2023-02-18 16:36:12 +0800

    Update CustomButton and AllProperties components

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { Button } from '@pankod/refine-mui';
+import { CustomButtonProps } from 'interfaces/common';
 
-function CustomButton() {
-  return <div>CustomButton</div>;
+function CustomButton({
+  type,
+  title,
+  backgroundColor,
+  color,
+  fullWidth,
+  icon,
+  disabled,
+  handleClick,
+}: CustomButtonProps) {
+  return (
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : 'unset',
+        width: fullWidth ? '100%' : 'content-fit',
+        padding: '10px 15px',
+        minWidth: 30,
+        fontSize: 16,
+        fontWeight: 600,
+        gap: '10px',
+        textTransform: 'capitalize',
+        backgroundColor,
+        color,
+        '&:hover': {
+          opacity: 0.9,
+          backgroundColor,
+        },
+      }}
+      onClick={handleClick}
+    >
+      {icon}
+      {title}
+    </Button>
+  );
 }
 
 export default CustomButton;
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { Add } from '@mui/icons-material';
+import { useList } from '@pankod/refine-core';
+import { Box, Stack, Typography } from '@pankod/refine-mui';
+import { useNavigate } from '@pankod/refine-react-router-v6';
+
+/* Components */
+import { PropertyCard, CustomButton } from 'components';
 
 function AllProperties() {
-  return <div>AllProperties</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignContent="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          All Properties
+        </Typography>
+        <CustomButton
+          title="Add Property"
+          handleClick={() => {}}
+          backgroundColor="#475be8"
+          color="#FCFCFC"
+          icon={<Add />}
+        />
+      </Stack>
+    </Box>
+  );
 }
 
 export default AllProperties;
