0385495371bd1541f349c5612c03f30300e46b81
Francis Ong <francis.ong@stratpoint.com>
2023-02-20 20:26:02 +0800

    Add connectDB file to src/helpers to establish connection with Mongo DB.

---
+++ src/helpers/connectDB.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url: string) => {
+  mongoose.set('strictQuery', true);
+
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log('Mongo DB connected');
+    })
+    .catch((error) => console.log(error));
+};
+
+export default connectDB;
