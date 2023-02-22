c3cdada3903681587528f88719991c5c562d9b52
Francis Ong <francis.ong@stratpoint.com>
2023-02-22 22:16:34 +0800

    Update user controller to add create user functionality and modify existing endpoints.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,16 +1,30 @@
 import { Request, Response } from 'express';
-// import userModel from '../models/user.js';
+import User from '../models/user.js';
 
-const createUser = (req: Request, res: Response) => {
-  console.log('This is a create user endpoint');
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { email, name, avatar } = req.body;
+    const userData = await User.findOne({ email });
+
+    if (userData) {
+      return res.status(200).json({ message: 'User Login Success', ...userData });
+    } else {
+      const newUser = await User.create({ name, email, avatar });
+
+      return res.status(201).json({ newUser, message: 'User Created' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
 };
 
 const getAllUsers = (req: Request, res: Response) => {
-  console.log('This is a get all users endpoint');
+  res.status(200).json({ message: 'This is a get all users endpoint' });
 };
 
 const getUserById = (req: Request, res: Response) => {
-  console.log('This is a getUserById endpoint');
+  res.status(200).json({ message: 'This is a getUserById endpoint' });
 };
 
 export { createUser, getAllUsers, getUserById };
