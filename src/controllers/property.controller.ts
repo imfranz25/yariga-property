47b5864464648bf1a45e17dd0f93a9c2d2db87d0
Francis Ong <francis.ong@stratpoint.com>
2023-02-26 17:11:18 +0800

    feat: add logic for ccreate property controller

--- src/controllers/property.controller.ts
+++ src/controllers/property.controller.ts
@@ -1,12 +1,47 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import User from '../models/user.js';
+import Property from '../models/property.js';
 import errorType from '../errors/errorType.js';
 import isError from '../helpers/isError.js';
-// import propertyModel from '../models/property.js';
-// import userModel from '../models/user.js';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
 
-const createProperty = (req: Request, res: Response) => {
+/* Initialization */
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const createProperty = async (req: Request, res: Response) => {
   try {
-    const { title, description, propertyType, location, price, photo } = req.body;
+    const { title, description, propertyType, location, price, photo, email } = req.body;
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const user = await User.findOne({ email }).session(session);
+
+    if (!user) {
+      return errorType.USER_NOT_FOUND('Email is not registered');
+    }
+
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    const newProperty = await Property.create({
+      title,
+      description,
+      propertyType,
+      location,
+      price,
+      photo: photoUrl.url,
+      creator: user._id,
+    });
+
+    user.allProperties.push(newProperty._id);
+    await user.save({ session });
+    await session.commitTransaction();
+
+    res.status(201).json({ message: 'Property Created Successfully' });
   } catch (error) {
     if (isError(error)) {
       errorType.SERVER_ERROR(error.message);
